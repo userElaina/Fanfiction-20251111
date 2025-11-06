51049873e63b709637e1d1da8bc76c8fc1d6d33f
userElaina <userelaina@pm.me>
2025-11-06 13:34:00 +0000

    :sparkles: screenshot ocr

---
+++ ocr.py
@@ -0,0 +1,42 @@
+import re
+import sys
+import time
+import easyocr
+import keyboard
+import numpy as np
+from PIL import ImageGrab
+
+ZH_PATTERN = r'[\u4e00-\u9fa5]+'
+
+# reader = easyocr.Reader(['ch_sim'])
+reader = easyocr.Reader(['ch_sim', 'en'])
+
+def ocr(img: np.ndarray) -> str:
+    return reader.readtext(img)
+
+def f1() -> None:
+    print('[Meta Shift S]')
+    time.sleep(5)
+
+    print('get img...')
+    img = ImageGrab.grabclipboard()
+    while img is None:
+        img = ImageGrab.grabclipboard()
+    data = np.array(img)
+
+    print('get str...')
+    l = list()
+    for idx, s, acc in ocr(data):
+        # matches = re.findall(ZH_PATTERN, s)
+        # l += matches
+        l += s
+    print('ocr: %s' % ''.join(l))
+    print()
+
+if __name__ == '__main__':
+    print('start')
+    while True:
+        if keyboard.is_pressed('ctrl+c'):
+            sys.exit(0)
+        if keyboard.is_pressed('windows+shift+s'):
+            f1()
